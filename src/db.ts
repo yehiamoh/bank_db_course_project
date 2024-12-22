38027d20795826bd50731a2fb389fa01346aa167
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-12-22 03:00:31 +0200

    tst db

---
+++ src/db.ts
@@ -0,0 +1,7 @@
+import { Client } from "pg";
+
+const client = new Client({
+   connectionString: process.env.DATABASE_URL,
+});
+client.connect();
+export default client;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+
+import client from './db'; './database';
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.get('/', (req, res) => {
   res.json('Hello World!');
+});
+app.get('/users', async (req, res) => {
+  try {
+    const result = await client.query('SELECT * FROM customer');
+    res.json(result.rows);
+  } catch (error:any) {
+    res.status(500).json({ error: error.message });
+  }
 });
 export default app;
