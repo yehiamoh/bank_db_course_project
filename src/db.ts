d0b7731127e367006a56b881d0ba466bbfb9820f
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-12-22 19:30:03 +0200

    test II

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,18 @@
 import { Client } from "pg";
 import dotenv from "dotenv";
 
-let client: Client | null = null;
+
 dotenv.config();
-console.log("DB URL : ",process.env.DATABASE_URL);
+
+const connectionString = process.env.DATABASE_URL;
+console.log(connectionString);
+
+let client: Client | null = null;
 const getClient = () => {
   if (!client) {
     client = new Client({
-      connectionString: process.env.DATABASE_URL,
+      connectionString: connectionString,
+      
     });
     client.connect();
   }
