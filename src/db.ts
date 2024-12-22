8a45f0a02bc53542d41f7c685abaeed7ab6cac36
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-12-22 19:25:59 +0200

    test I

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,9 @@
 import { Client } from "pg";
+import dotenv from "dotenv";
 
 let client: Client | null = null;
-
+dotenv.config();
+console.log("DB URL : ",process.env.DATABASE_URL);
 const getClient = () => {
   if (!client) {
     client = new Client({
